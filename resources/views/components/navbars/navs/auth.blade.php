05f4c2be3fa7d7f981748482ce952f2b0b7a77f9
Roberto <bryan.sosof@gmail.com>
2022-10-28 16:53:23 -0600

    Fix: options users

--- resources/views/components/navbars/navs/auth.blade.php
+++ resources/views/components/navbars/navs/auth.blade.php
@@ -2,27 +2,17 @@
      navbar-scroll="true">
     <div class="container-fluid py-1 px-3">
         <nav aria-label="breadcrumb">
+            <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
+                <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="javascript:;">Pages</a></li>
+                <li class="breadcrumb-item text-sm text-dark active text-capitalize"
+                    aria-current="page">{{ str_replace('-', ' ', Route::currentRouteName()) }}</li>
+            </ol>
             <h6 class="font-weight-bolder mb-0 text-capitalize">{{ str_replace('-', ' ', Route::currentRouteName()) }}</h6>
         </nav>
         <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
             <div class="ms-md-auto pe-md-3 d-flex align-items-center">
             </div>
-            <form method="POST" action="" class="d-none" id="logout-form">
-                @csrf
-            </form>
             <ul class="navbar-nav  justify-content-end">
-                <li class="nav-item d-flex align-items-center">
-                    <a href="{{ route('user-profile')}}" class="nav-link text-body font-weight-bold px-0"
-                       data-bs-toggle="tooltip" data-bs-placement="top" title="Profile"
-                    >
-                        <i class="fa-solid fa-user-gear me-sm-1"></i>
-                    </a>
-                </li>
-                <li class="nav-item d-flex align-items-center">
-                    <a href="javascript:;" class="nav-link text-body font-weight-bold px-0">
-                        <livewire:auth.logout/>
-                    </a>
-                </li>
                 <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
                     <a href="javascript:;" class="nav-link text-body p-0" id="iconNavbarSidenav">
                         <div class="sidenav-toggler-inner">
@@ -36,6 +26,37 @@
                     <a href="javascript:;" class="nav-link text-body p-0">
                         <i class="fa fa-cog fixed-plugin-button-nav cursor-pointer"></i>
                     </a>
+                </li>
+
+
+                <li class="nav-item dropdown pe-2 d-flex align-items-center">
+                    <a href="javascript:;" class="nav-link text-body p-0" id="dropdownMenuButton"
+                       data-bs-toggle="dropdown" aria-expanded="false">
+                        <img class="rounded-circle" style="width: 32px" src="{{ auth()->user()->google_avatar }}"
+                             alt=""/>
+                    </a>
+                    <ul class="dropdown-menu  dropdown-menu-end  px-2 py-3 me-sm-n4"
+                        aria-labelledby="dropdownMenuButton">
+                        <li class="mb-2">
+                            <a class="dropdown-item border-radius-md" href="{{ route('user-profile')}}">
+                                <div class="d-flex py-1">
+                                    <div class="my-auto">
+                                        <i class="fa-solid fa-user-gear me-sm-1"></i>
+                                        Profile
+                                    </div>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="mb-2">
+                            <a class="dropdown-item border-radius-md" href="javascript:;">
+                                <div class="d-flex py-1">
+                                    <div class="my-auto">
+                                        <livewire:auth.logout/>
+                                    </div>
+                                </div>
+                            </a>
+                        </li>
+                    </ul>
                 </li>
             </ul>
         </div>
